1baef58fecd240ec3eb6d8df3e4ebc9e79d993f8
Roderick Romero <rjrr507@gmail.com>
2021-07-08 21:45:29 -0500

    cambios paquetes de seguridad

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-require("dotenv").config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
+const helmet = require("helmet");
 
-
+const { logger, requestLogger, errorLogger } = require("./utils/logger");
 
 const app = express();
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
+const VIEW_DIR = path.join(__dirname, "views");
+const VIEW_PARTIALS = path.join(__dirname, "views/partials");
 
+hbs.registerPartials(VIEW_PARTIALS);
 
-app.listen(SERVER_PORT)
+app.use(helmet());
+
+app.use(requestLogger);
+
+app.set("views", VIEW_DIR);
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home page" });
+});
+
+app.use(errorLogger);
+
+app.listen(SERVER_PORT, () => {
+  logger.info(`Servidor Iniciado en puerto ${SERVER_PORT}`);
+});
+
+process.on("error", (err) => {
+  console.error("Erro capturaDO", err);
+});
+
---
+++ utils/logger.js
@@ -0,0 +1,58 @@
+"use strict";
+const winston = require("winston");
+const remoteLog = new winston.transports.Http({
+  host: "localhost",
+  port: 3001,
+  path: "/errors",
+});
+
+const consoleLog = new winston.transports.Console();
+
+const myWinstonOptions = {
+  transports: [consoleLog],
+};
+const logger = new winston.createLogger(myWinstonOptions);
+
+module.exports = {
+  logger,
+  requestLogger: createRequestLogger([consoleLog]),
+  errorLogger: createErrorLogger([remoteLog, consoleLog]),
+};
+
+function createRequestLogger(transports) {
+  const requestLogger = winston.createLogger({
+    format: getRequestLogFormatter(),
+    transports: transports,
+  });
+
+  return function logRequest(req, res, next) {
+    requestLogger.info({ req, res });
+    next();
+  };
+}
+
+function createErrorLogger(transports) {
+  const errLogger = winston.createLogger({
+    level: "error",
+    transports: transports,
+  });
+
+  return function logError(err, req, res, next) {
+    errLogger.error({ err, req, res });
+    next();
+  };
+}
+
+function getRequestLogFormatter() {
+  const { combine, timestamp, printf } = winston.format;
+
+  return combine(
+    timestamp(),
+    printf((info) => {
+      const { req, res } = info.message;
+      return `${info.timestamp} ${info.level}: ${req.hostname}${
+        req.port || ""
+      }${req.originalUrl}`;
+    })
+  );
+}
